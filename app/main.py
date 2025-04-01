0ff657f18456342da59d7bc51bdc06391fc6434c
santi <santialbus@gmail.com>
2025-04-01 11:57:52 +0200

    addd cors

--- app/main.py
+++ app/main.py
@@ -3,8 +3,22 @@ from app.routers.data_router import router as data_router
 from app.routers.user_router import router as user_router
 from app.routers.favorite_station_router import router as favorite_station_router
 from app.database import init_db  # Asegúrate de importar la función init_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",  # Add your Flutter app's origin if different
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (including OPTIONS)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Incluyendo los routers
 app.include_router(data_router)
